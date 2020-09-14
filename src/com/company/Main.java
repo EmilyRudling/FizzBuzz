4c65ff09e8b2fc9959d9b31afb0e18a973eda875
Emily Rudling <mmrudling@yahoo.com>
2020-09-14 14:56:17 +0100

    Finished activity 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Welcome to fizzbuzz");
-        int i = 0;
-        while (i != 100) {
+        int i = 1;
+        while (i <= 100) {
             FizzBuzz(i);
             i++;
         }
     }
     public static void FizzBuzz(int i){
-        if (i % 5 = 0 && i % 3 = 0){
-
+        if (i % 5 == 0 && i % 3 == 0){
+            System.out.println("FizzBuzz");
+        }
+        else if (i % 3 == 0){
+            System.out.println("Fizz");
+        }
+        else if (i % 5 == 0){
+            System.out.println("Buzz");
+        }
+        else{
+            System.out.println(i);
         }
     }
 }
