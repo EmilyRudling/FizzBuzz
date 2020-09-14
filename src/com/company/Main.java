aecb49213d8dc7b9014c68ffe93f475f6b80290f
Emily Rudling <mmrudling@yahoo.com>
2020-09-14 14:18:58 +0100

    INFORMATIVE COMMIT MESSAGE HERE

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,16 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("Welcome to fizzbuzz");
+        int i = 0;
+        while (i != 100) {
+            FizzBuzz(i);
+            i++;
+        }
+    }
+    public static void FizzBuzz(int i){
+        if (i % 5 = 0 && i % 3 = 0){
+
+        }
     }
 }
