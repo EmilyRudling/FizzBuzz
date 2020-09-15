fd082fab8c9d1fa7ba365f126b7d08df3e6ef0eb
Emily Rudling <mmrudling@yahoo.com>
2020-09-15 14:11:31 +0100

    Need to work out Fezz

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,21 @@
 package com.company;
+import java.util.*;
 import java.util.Scanner;  // imports the Scanner class
 //https://isolate.menlosecurity.com/https://corndel.atlassian.net/wiki/spaces/AC/pages/23003252/1+-+FizzBuzz+Java
 
-//STILL TO DO"
-// If a number is a multiple of 17, reverse the order in which any fizzes,
-// buzzes, bangs etc. are printed. (E.g. 3 * 5 * 17 = 255: "BuzzFizz")
-
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("Please enter a maximum number: ");
         Scanner scanner = new Scanner(System.in);
         int i = Integer.parseInt(scanner.nextLine());
         int x = 1;
         while (x <= i) {
-            FizzBuzz(x);
+            FizzBuzz2(x);
             x++;
         }
     }
-    public static void FizzBuzz(int i){
+
+    public static void FizzBuzz(int i){ //BAD BAD BAD - VERSION 1
         if (i % 5 == 0 || i % 3 == 0 || i % 11 == 0 || i % 7 == 0 || i % 13 == 0){
             //all of the numbers that should be replaced are checked above
             if (i % 11 == 0){
@@ -49,5 +46,36 @@ public class Main {
         else{
             System.out.println(i);
         }
+    }
+
+    public static void FizzBuzz2(int i) { //nice and nice -- VERSION 2
+        List outputArray = new ArrayList();
+        if (i % 3 == 0){
+            outputArray.add("Fizz");
+        }
+        if (i % 5 == 0){
+            outputArray.add("Buzz");
+        }
+        if (i % 7 == 0){
+            outputArray.add("Bang");
+        }
+        if (i % 11 == 0){
+            outputArray.clear();
+            outputArray.add("Bong");
+        }
+        if (i % 13 == 0){
+            outputArray.add("Fezz"); //BEFORE FIRST B WORD
+
+        }
+        if (i % 17 == 0){
+            Collections.reverse(outputArray);
+        }
+        if (outputArray.isEmpty()){
+            outputArray.add(Integer.toString(i));
+        }
+        for(Object val : outputArray) {
+            System.out.print(val);
+        }
+        System.out.println("");
     }
 }
